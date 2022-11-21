79d5f2e752f9766d71a2ac9e6bb57fd39d2cbe52
zyj <1041586971@qq.com>
2022-11-21 17:53:21 +0800

    feat：新增 软连接 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import { resolve } from 'path';
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  resolve: {
+    // https://cn.vitejs.dev/config/shared-options.html#resolve-alias
+    alias: [
+      // /@/xxxx => src/xxxx
+      {
+        find: /\/@\//,
+        replacement: pathResolve('src') + '/',
+      },
+    ],
+  },
 });
