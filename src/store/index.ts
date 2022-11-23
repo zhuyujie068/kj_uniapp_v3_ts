f052bf750a627e9f0dec620a2afa04ed4cfcb391
zyj <1041586971@qq.com>
2022-11-23 18:03:51 +0800

    feat：新增 pinia-plugin-persistedstate 进行数据的持久化

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,25 @@
 import type { App } from 'vue';
 import { createPinia } from 'pinia';
+// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { createPersistedState } from 'pinia-plugin-persistedstate'  // 暂时使用 pinia-plugin-persistedstate 看其他平台兼容性怎么样，可能会修改成 pinia-plugin-persist-uni
 
 const store = createPinia();
+// store.use(piniaPluginPersistedstate)
+store.use(
+  // 使用 uniapp 的 api 进行数据存取 https://blog.csdn.net/weixin_47077674/article/details/124684551
+  createPersistedState({
+    storage: {
+      getItem(key: string): string | null {
+        debugger
+        return uni.getStorageSync(key)
+      },
+      setItem(key: string, value: string) {
+        uni.setStorageSync(key, value)
+      }
+    }
+  })
+)
+
 
 export function setupStore(app: App<Element>) {
   app.use(store);
--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -1,18 +1,25 @@
 import { defineStore } from 'pinia';
 
 export const useAppStore = defineStore({
-    id: 'app-store',
-    state: () => ({
-      title: 'Hello world！',
-    }),
-    getters: {
-        getTitle(): string {
-            return this.title;
-        }
-    },
-    actions: {
-        changeTitle() {
-            this.title = '你好，世界！';
-        }
+  id: 'app-store',
+  state: () => ({
+    title: 'Hello world！',
+    version: '0.0.1',
+  }),
+  getters: {
+    getTitle(): string {
+      return this.title;
     }
+  },
+  actions: {
+    changeTitle() {
+      this.title = '你好，世界！';
+    }
+  },
+  // 开启数据缓存，去掉 strategies 就是当前 store 里的 state 对象中的数据全部进行缓存
+  persist: {
+    key:'appStore',
+    // storage: sessionStorage,   // 默认的是使用 localstorage 进行数据存储
+    paths: ['title','version'],
+  },
 })
