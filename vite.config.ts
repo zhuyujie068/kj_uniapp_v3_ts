5f763f1d8d822c0557d4d63a943a3a21271d2668
zyj <1041586971@qq.com>
2022-11-24 17:05:04 +0800

    feat：vite config 添加 server 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import { resolve } from 'path';
 import WindiCSS from 'vite-plugin-windicss'
@@ -8,7 +8,7 @@ function pathResolve(dir: string) {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default ({ mode }) => defineConfig({
   plugins: [
     WindiCSS({
       scan: {
@@ -28,4 +28,19 @@ export default defineConfig({
       },
     ],
   },
+  // https://cn.vitejs.dev/config/server-options.html#server-proxy
+  server: {
+    port: Number(loadEnv(mode, process.cwd()).VITE_PORT),
+    open: true, //自动打开
+    base: './ ', //生产环境路径
+    proxy: {
+      // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
+      // 正则表达式写法
+      '^/api': {
+        target: loadEnv(mode, process.cwd()).VITE_PROXY_API, // 后端服务实际地址
+        changeOrigin: true, //开启代理
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  }
 });
